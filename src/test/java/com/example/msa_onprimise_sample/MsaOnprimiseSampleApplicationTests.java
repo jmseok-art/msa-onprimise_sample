9d52e6f56a71fce962b77286eaf6edf27c8b229b
Jeong Min Seok <jmseok@enterprise1.co.kr>
2025-10-29 14:21:03 +0900

    feat: 4단계 - product-service에 /products API 구현 - GET /products: 더미 상품 리스트 응답 - DTO ProductDto 생성 (id, name, price)

---
+++ src/test/java/com/example/msa_onprimise_sample/MsaOnprimiseSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msa_onprimise_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaOnprimiseSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
