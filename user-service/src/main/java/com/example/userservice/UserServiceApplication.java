7027f5b2eb2f0266f85db4e1d4032e40c55884aa
Jeong Min Seok <jmseok@enterprise1.co.kr>
2025-11-19 11:18:02 +0900

    fix: 의존성 정리

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,11 +2,9 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @ComponentScan(basePackages = {
     "com.example.userservice",    
     "com.example.common"           
