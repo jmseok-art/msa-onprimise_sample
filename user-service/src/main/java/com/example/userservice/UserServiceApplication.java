ec806650558e132fbcb5fd50f63709fafbace8f6
Jeong Min Seok <jmseok@enterprise1.co.kr>
2025-11-25 17:33:15 +0900

    fix: build.gradle 수정

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
     "com.example.userservice",    
     "com.example.common"           
 })
+@EnableDiscoveryClient
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
